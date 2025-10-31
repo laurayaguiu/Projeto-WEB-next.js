d2ecd78607f815c3c280210ecac25e87ccc87515
Stephanie Julia Soares Dias <ssoaresdias9802@gmail.com>
2025-10-31 20:30:59 +0000

    fazendo css do nav funcionar

--- projeto/src/app/layout.js
+++ projeto/src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import Link from 'next/link';
-import Nav from './components/Nav';
+import Nav from './components/nav/Nav';
 import Footer from './components/Footer';
 
 export const metadata = {
